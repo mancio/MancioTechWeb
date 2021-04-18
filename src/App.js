34f44cf8aef7f57c228451e0d15b4bedbde7822d
Andrea Mancini <mancioman2@gmail.com>
2021-04-18 22:32:49 +0200

    basic layout works!

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import './App.css';
 import Pacman from "./mainpage/pacman/Pacman";
 import Title from "./mainpage/title/Title";
+import Enter from "./mainpage/enter/Enter";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <Title></Title>
-      <Pacman></Pacman>
+      <header className="App-main-container">
+        <Title/>
+        <Pacman/>
+        <Enter/>
       </header>
     </div>
   );
---
+++ src/mainpage/enter/Enter.js
@@ -0,0 +1,12 @@
+import logo from './Enter.svg'
+import './Enter.css'
+
+function Enter(){
+    return(
+        <div>
+            <img src={logo} className='Enter'/>
+        </div>
+    )
+}
+
+export default Enter;
