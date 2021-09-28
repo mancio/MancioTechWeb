e60d40f9a5a874b76493c4cb6d5041b16b2488fe
Andrea Mancini <mancioman2@gmail.com>
2021-09-28 17:14:55 +0200

    - test need redirection control - new 404 not found page

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const Platform = React.lazy(() => import("./series/Platform"));
 const Weather = React.lazy(() => import("./weather/Weather"));
 const Recipes = React.lazy(() => import("./recipes/Recipes"));
 const SingleRecipe = React.lazy(() => import("./recipes/SingleRecipe"));
+const NotFound = React.lazy( () => import('./notfound/NotFound'));
 
 class App extends React.Component{
 
@@ -45,6 +46,7 @@ class App extends React.Component{
                 <Route path='/recipes' component={Recipes}/>
                 <Route path='/recipe/:tag' component={SingleRecipe}/>
                 <Route path='/boo' component={WorkInProgress}/>
+                <Route component={NotFound}/>
               </Switch>
             </Router>
           </React.Suspense>
---
+++ src/notfound/NotFound.js
@@ -0,0 +1,8 @@
+export default function NotFound(){
+    return(
+        <div>
+            <p>404 - NOT FOUND</p>
+            <p>Please check the URL</p>
+        </div>
+    )
+}
