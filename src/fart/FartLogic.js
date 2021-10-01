376240bb811bfc372b77c55e324b9567f3c5dbb8
Andrea Mancini <mancioman2@gmail.com>
2021-10-01 12:59:36 +0200

    fart page!

--- src/fart/FartLogic.js
+++ src/fart/FartLogic.js
@@ -1,6 +1,6 @@
-import $ from 'jquery';
+// inspired by http://jsfart.com
 
-let farts = {
+const farts = {
 	toot : 'fart1',
 	ripper : 'fart2',
 	plop : 'fart3',
@@ -17,140 +17,34 @@ let farts = {
 	fartpoint1: 'fart14'
 };
 
+const valuesOfFarts = Object.values(farts);
 
+const namesOfFarts = Object.keys(farts);
 
-
-/**
-* This is the Fart constructor.
-*
-* @class FartLogic
-* @constructor
-* 
-* @param {Object} options Default Sound, Loop and Volume 
-*/
-export function FartLogic(options) {
-	this.default_sound = undefined;
-	this.sound = this.default_sound;
-	this.fart_player = null;
-	this.old_player = false;
-	this.options = this.fartxtend({
-		default_sound : farts.raspberry,
-		loop : false,
-		volume : 50	// 0 - 100
-	}, options);
-	this.init();
+export const getNamesOfFarts = function (){
+	return namesOfFarts;
 }
-FartLogic.preloaded = false;
 
-/**
- * 
- * Mimic jQuery Extend. 			✓ 
- * Change name for Fart-based LOL. 	✓   
- * 
- */
-FartLogic.prototype.fartxtend = function(options, defaults) {
-    for(let key in defaults){
-        if(defaults.hasOwnProperty(key)){
-        	options[key] = defaults[key];
-        }    	
-    }   
-    return options;
-};
+const getFartNameFromIndex = function (index){
+	return namesOfFarts[index];
+}
 
-/**
-* Init the player. Figure out if the old player should be loaded by checking if the audio element has the canPlayType() method 
-*
-* @method init
-*/
-FartLogic.prototype.init = function() {
-	this.fart_player = document.createElement("audio");
-	if ( typeof (this.fart_player.canPlayType) == 'undefined') {
-		this.load_old_player();
-	}
-	this.preload();
-};
+const getIndexFromFartName = function (name){
+	return namesOfFarts.indexOf(name);
+}
 
-/**
-* If the browser is too old to load an AUDIO element, its probably ie7/8 
-*
-* @method load_old_player
-*/
-FartLogic.prototype.load_old_player = function() {
-	document.body.innerHTML += '<div style="display:none;"><object id="contentPlayer" classid="CLSID:6BF52A52-394A-11d3-B153-00C04F79FAA6" width="100" height="100"><param name="volume" value="100%" /><param name="windowlessVideo" value="true"><param name="AnimationatStart" value="0" /><param name="autostart" value="1" /></object></div>';
-	this.fart_player = document.getElementById('contentPlayer');
-	this.fart_old_player = true;
-};
+const playFart = function (index){
+	const audio = new Audio('https://raw.githubusercontent.com/74656c/fart.js/master/farts/'+ valuesOfFarts[index] + '.mp3');
+	audio.play().then(r => console.log("I'm farting!: " + getFartNameFromIndex(index)))
+		.catch(r => console.log(r));
+}
 
-/**
-* Play the sound via the browser. 
-* If a valid $sound is passed, it will play, otherwise it will default to the defaut sound
-*
-* @method play
-* @param {String} foo The name of the sound to play
-* @param {String} foo A callback for once the sound has ended
-*/
-FartLogic.prototype.play = function(sound, callback) {
-	var fart = (sound) ? (farts[sound]) ? farts[sound] : sound: this.options.default_sound;
-	callback = callback || null;
-	if (!this.fart_old_player) {
-		var ext = (this.fart_player.canPlayType('audio/mp3')) ? '.mp3' : '.wav';
-		this.fart_player.setAttribute('src', "/farts/" + fart + ext);
-		this.fart_player.loop = this.options.loop;
-		this.fart_player.volume = (this.options.volume / 100);
-		this.fart_player.play();
-		$(this.fart_player).on("ended", function() {
-			if (callback) {
-				callback();
-				$(this.fart_player).off("ended");
-			}
-		});
-	} else {
-		this.fart_player.URL = "/farts/" + fart + '.mp3';
-	}
-};
+export const playRandomFart = function (){
+	const rand = Math.floor(Math.random() * valuesOfFarts.length);
+	playFart(rand);
+}
 
-/**
-* Stop the sound from playing 
-*
-* @method stop
-*/
-FartLogic.prototype.stop = function() {
-	this.fart_player.pause();
-};
-
-/**
-* Remove the element from the DOM 
-*
-* @method remove
-*/
-FartLogic.prototype.remove = function() {
-	this.fart_player.remove();
-};
-
-/**
-* Play a random fart from the list 
-*
-* @method random
-*/
-FartLogic.prototype.random = function() {
-	var keys = Object.keys(farts);
-	var fart = keys[keys.length * Math.random() << 0];
-	this.play(fart);
-};
-
-/**
-* Try and preload the sounds. This was useful when the sound files where on a remote server
-*
-* @method preload
-*/
-FartLogic.prototype.preload = function() {
-	var fart_player = this.fart_player;
-	if (!this.fart_old_player && !FartLogic.preloaded) {
-		for(let f in farts){
-			var ext = (fart_player.canPlayType('audio/mp3')) ? '.mp3' : '.wav';
-			fart_player.setAttribute('src', "/farts/" + farts[f] + ext);
-			console.log('preload:',farts[f]);
-		}
-		FartLogic.preloaded = true;
-	}
-};
+export const playSingleFart = function (name){
+	const index = getIndexFromFartName(name);
+	playFart(index);
+}
--- src/fart/Farts.js
+++ src/fart/Farts.js
@@ -2,27 +2,33 @@ import './Farts.css';
 import ButtonTemplate from "../menu/ButtonTemplate";
 import React from "react";
 import {getMenuItemByTag} from "../menu/MenuHandler";
-import {FartLogic} from "./FartLogic";
+import {getNamesOfFarts, playRandomFart, playSingleFart} from "./FartLogic";
 
 export default function Farts(){
 
     const backButton = getMenuItemByTag('back');
 
 
-    function fartClick(){
-        const fart = new FartLogic();
-        fart.play();
-    }
-
     return(
-        <div>
+        <div className='fart-page'>
             <h1 className='fart-title'>🤣 🤣 Farting page 🤣 🤣</h1>
             <h3 className='fart-desc'>
                 If you ever wonder if a JS library to make your page fart exist....
                 well YES!
             </h3>
-            <p><a href='http://jsfart.com' rel="noreferrer" target="_blank">CLicK HeRe!</a> to visit the library </p>
-            <button onClick={fartClick} className='fart-rand-button'>Random Fart</button>
+            <p>
+                Unfortunately the famous jsFart library is not maintained anymore so I decided to write my own code in
+                much more simple "React Style" way. <a href='http://jsfart.com' rel="noreferrer" target="_blank">CLicK HeRe!</a> to visit
+                the old library or visit my <a href='https://github.com/mancio/MancioTechWeb'>GitHub</a> for the new implementation.
+            </p>
+            <div className='fart-control-panel'>
+                <button onClick={() => playRandomFart()} className='fart-button'>Random Fart</button>
+                {getNamesOfFarts().map(name => {
+                    return(
+                        <button onClick={() => playSingleFart(name)} className='fart-button'>{name}</button>
+                    )
+                })}
+            </div>
             <ButtonTemplate
                 key={backButton.id}
                 id={backButton.id}
