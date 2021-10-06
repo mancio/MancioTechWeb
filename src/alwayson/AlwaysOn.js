62538a233bd0ab71dddc244f894413f429bc2d63
Andrea Mancini <mancioman2@gmail.com>
2021-10-06 19:01:36 +0200

    always on with mp4 loop

--- src/alwayson/AlwaysOn.js
+++ src/alwayson/AlwaysOn.js
@@ -1,12 +1,23 @@
+import React from 'react'
+import ReactPlayer from 'react-player'
 import './AlwaysOn.css';
-import { useKeepAwake } from '@sayem314/react-native-keep-awake';
 
 export default function AlwaysOn(){
-    useKeepAwake();
     return(
         <div className='always-on'>
             <h1>Keep this page open to keep the display always on</h1>
             <p>You know.... sometimes bad people want to control the behavior of the display of your PC</p>
+            <div className='player-wrapper-always-on'>
+                <ReactPlayer
+                    className='react-player-always-on'
+                    url='https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4'
+                    width='100%'
+                    height='100%'
+                    playing={true}
+                    loop={true}
+                    controls={false}
+                />
+            </div>
         </div>
     )
 }
