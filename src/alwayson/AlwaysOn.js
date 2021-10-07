7bf95508d825ef196693863271fb2a8adb3ac809
Andrea Mancini <mancioman2@gmail.com>
2021-10-07 12:39:09 +0200

    try to prevent screen sleep

--- src/alwayson/AlwaysOn.js
+++ src/alwayson/AlwaysOn.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import ReactPlayer from 'react-player'
 import './AlwaysOn.css';
 
 export default function AlwaysOn(){
+
+    const [visible, setVisible] = useState(true);
+
+    useEffect(()=>{
+        if(visible){
+            setTimeout(()=>{
+                setVisible(false);
+            },10000);
+        }else {
+            setTimeout(()=>{
+                setVisible(true);
+            },200);
+        }
+    },[visible]);
+
     return(
         <div className='always-on'>
             <h1>Keep this page open to keep the display always on</h1>
             <p>You know.... sometimes bad people want to control the behavior of the display of your PC</p>
-            <div className='player-wrapper-always-on'>
+            {visible && (
+                <div className='player-wrapper-always-on'>
                 <ReactPlayer
                     className='react-player-always-on'
                     url='https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4'
@@ -17,7 +33,13 @@ export default function AlwaysOn(){
                     loop={true}
                     controls={false}
                 />
-            </div>
+                </div>
+            )}
+            {!visible && (
+                <div>
+                    <p>Loading</p>
+                </div>
+            )}
         </div>
     )
 }
