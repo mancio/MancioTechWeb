1de0833d7b0fce1ab251d7282e32f9de6f0814fa
Andrea Mancini <mancioman2@gmail.com>
2021-10-18 17:25:15 +0200

    time now update from internet

--- src/mainpage/timenow/TimeNow.js
+++ src/mainpage/timenow/TimeNow.js
@@ -1,23 +1,34 @@
 import './TimeNow.css';
-import {getMyIP} from "./TimeNowHandler";
+import {addSecToUnixTime, getMyIP, getRealTime, getGeoInfo} from "./TimeNowHandler";
 import {useEffect, useState} from "react";
 
 export default function TimeNow(){
 
-    const [ip, setIp] = useState('ppp');
+    const [ip, setIp] = useState('0');
+    const [time, setTime] = useState('0');
+    const [city, setCity] = useState('null');
+    const [country, setCountry] = useState('null');
 
     useEffect(() => {
-        fetch('https://api.ipify.org?format=json')
-            .then(r => r.json())
-            .then(d => {
-                console.log(d.ip);
-                setIp(d.ip);
-            }).catch(err => console.log(err));
-    },[ip]);
+        if (ip === '0') getMyIP().then(r => setIp(r));
+        if (ip !== '0' && time === '0') getGeoInfo(ip).then(geo => {
+            setTime(geo.time);
+            setCity(geo.city);
+            setCountry(geo.country);
+        });
+        if (time !== '0'){
+            setTimeout(()=>{
+                setTime(addSecToUnixTime(time));
+            },1000);
+        }
+    },[ip, time, city, country]);
 
     return(
         <div>
-            <p>{ip}</p>
+            <p>
+                {time === '0' ? 'time updating' : getRealTime(time)} in
+                your city {city} in {country}
+            </p>
         </div>
     )
 }
--- src/mainpage/timenow/TimeNowHandler.js
+++ src/mainpage/timenow/TimeNowHandler.js
@@ -1,9 +1,47 @@
+import axios from "axios";
+import {geoApiKey} from "../../passwords/GeoPassword";
+
 export const getMyIP = async function (){
-    fetch('https://api.ipify.org?format=json')
-        .then(r => r.json())
-        .then(d => {
-            console.log(d.ip);
-            return d.ip;
-        }).catch(err => console.log(err))
-        .finally(() => {return 0});
+    return axios.get('https://api.ipify.org?format=json')
+        .then(r => {
+            console.log(r.data.ip);
+            return r.data.ip;
+        })
+        .catch(error => {
+            console.log(error);
+            return '0';
+        })
+}
+
+export const getGeoInfo = function (ip){
+    return axios.get('https://api.ipgeolocation.io/ipgeo?apiKey=' + geoApiKey + '&ip=' + ip)
+        .then(r => {
+            console.log(r);
+            const time = r.data.time_zone.current_time_unix*1000;
+            const city = r.data.city;
+            const country = r.data.country_name;
+            return {time, city, country};
+        }).catch(error => {
+            console.log(error);
+        })
+}
+
+const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric' ,
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+};
+
+export const getRealTime = function (unixTime){
+    return new Date(unixTime).toLocaleDateString('en-GB', options);
+}
+
+export const addSecToUnixTime = function (unixTime){
+    const date = new Date(unixTime);
+    date.setSeconds(date.getSeconds()+1);
+    return date.getTime();
 }
