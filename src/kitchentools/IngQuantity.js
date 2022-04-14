810d6efc95aac179bc3bd1b046d7cadd30c09743
Andrea Mancini <mancioman2@gmail.com>
2022-04-14 17:08:26 +0200

    logic quantity counter ready, need better css

--- src/kitchentools/IngQuantity.js
+++ src/kitchentools/IngQuantity.js
@@ -1,12 +1,15 @@
 import React, {useEffect, useState} from "react";
 import "./Ktools.css";
-import {addAllIngSize, getAllIngSize} from "./KitchenLogic";
+import {addAllIng, checkIDs, getAllIngNames, getResMap, setAllReadOnly, updateAllIng} from "./KitchenLogic";
 
 export function IngQuantity(){
     const [elID, setElID] = useState(0);
     const [elem, setElem] = useState([]);
+    const [elOut, setElOut] = useState([]);
+    const [drop, setDrop] = useState([]);
     const [full, setFull] = useState(false);
     const [done, setDone] = useState(false);
+    const [result, setResult] = useState(false);
 
     useEffect(()=>{
         if (elID >= 2) setFull(true);
@@ -17,8 +20,9 @@ export function IngQuantity(){
         const ingSize = "ing-size-" + elID;
         return(
             <p>
-                <span><input className='hy-calc-input' type="text" name="ing" key={ingName} id={ingName} defaultValue='name'/></span>
-                <span><input className='hy-calc-input' type="number" name="ing" key={ingSize} id={ingSize} defaultValue='0'/></span>
+                <>{elID + 1}: </>
+                <span><input className='hy-calc-input' type="text" name={ingName} key={ingName} id={ingName} defaultValue={'name' + (elID + 1)}/></span>
+                <span><input className='hy-calc-input' type="number" name={ingSize} key={ingSize} id={ingSize} defaultValue='0'/></span>
                 <span> Quantity (gr, ml, ...)</span>
             </p>
         )
@@ -26,17 +30,39 @@ export function IngQuantity(){
 
     function addElement(){
         setElID(elID + 1);
-        setElem(elem.concat(<Input/>));
+        setElem(elem.concat(<Input key={elID}/>));
     }
 
     function start(){
-        addAllIngSize("ing-size-", elID);
+        let options = [];
+        addAllIng("ing-name-","ing-size-", elID);
+        let pos = 0;
+        getAllIngNames().forEach(n => {
+            options.push(<option key={pos} value={n}>{n}</option>);
+            pos++;
+        })
+        setDrop(options);
+        if(checkIDs(elID)){
+            setDone(true);
+            setFull(false);
+            setAllReadOnly("ing-name-", elID);
+            setAllReadOnly("ing-size-", elID);
+        } else window.alert("two or more ingredients are repeated pls check");
     }
 
-    function result(){
-        getAllIngSize().forEach(s => {
-
+    function update(){
+        let out = [];
+        const sel = document.getElementById('ingSel').value;
+        updateAllIng(sel, document.getElementById('newing').value)
+        getResMap().forEach((value, key) => {
+            out.push(
+                <p>{key} | {value}</p>
+            )
         })
+        setElOut(out);
+        setFull(false);
+        setDone(false);
+        setResult(true);
     }
 
     return(
@@ -46,7 +72,19 @@ export function IngQuantity(){
             <button className='cooking-tools-button' onClick={() => addElement()}>Add Ingredient</button>
             {elem}
             {full && <button className='cooking-tools-button' onClick={() => start()}>Start</button>}
-            {done && }
+            {done &&
+                <>
+                    <select id='ingSel'>{drop}</select>
+                    <p>New quantity for this ingredient</p>
+                    <input className='hy-calc-input' type="number" name='newing' id='newing' defaultValue='0'/>
+                    <button className='cooking-tools-button' onClick={() => update()}>Update All Ingredients</button>
+                </>
+            }
+            {result &&
+                <>
+                    {elOut}
+                </>
+            }
         </div>
     )
 }
--- src/kitchentools/KitchenLogic.js
+++ src/kitchentools/KitchenLogic.js
@@ -1,3 +1,10 @@
+const ingMap = new Map();
+const newIngMap = new Map();
+
+export const getResMap = function (){
+    return newIngMap;
+}
+
 export const verifyInputs = function (input, pos){
    for(let i = 0; i<input.length; i++){
        if(i !== pos && isNaN(input[i])) return false;
@@ -5,18 +12,40 @@ export const verifyInputs = function (input, pos){
    return true;
 }
 
-const size = [];
-
-export const getAllIngSize = function (){
-    return size;
-}
-
-const addIngSize = function (value) {
-    size.push(parseInt(value));
-}
-
-export const addAllIngSize = function (tag, elID){
-    for (let i = 0; i<=elID; i++){
-        addIngSize(document.getElementById(tag + elID).value)
+export const setAllReadOnly = function (tag, ids){
+    for (let i = 0; i<ids ;i++){
+        document.getElementById(tag + i).readOnly = true;
     }
+}
+
+export const getAllIngNames = function (){
+    return Array.from(ingMap.keys());
+}
+
+export const addAllIng = function (tagName, tagValue, elID){
+    for (let i = 0; i<elID; i++){
+        const name = String(document.getElementById(tagName + i).value);
+        const value = parseInt(document.getElementById(tagValue + i).value);
+        ingMap.set(name, value);
+    }
+}
+
+export const checkIDs = function (ids){
+    return Array.from(ingMap.keys()).length === ids;
+}
+
+export const difRatio = function (oldVal, newVal){
+    return parseInt(newVal)/parseInt(oldVal);
+}
+
+export const getOldVal = function (name){
+    return parseInt(ingMap.get(name));
+}
+
+export const updateAllIng = function (name, newVal){
+    console.log(getOldVal(name));
+    const ratio = difRatio(getOldVal(name), newVal);
+    getAllIngNames().forEach(el => {
+        newIngMap.set(el, ingMap.get(el) * ratio);
+    })
 }
