d46959632ff48ef7fa658a5d639aff12b456807e
Andrea Mancini <mancioman2@gmail.com>
2022-04-19 13:21:20 +0200

    html char filter fixed

--- src/logic/TextHandler.js
+++ src/logic/TextHandler.js
@@ -1,7 +1,13 @@
 export const replaceHtmlCharacters = function (string){
     return string.replace(/&#(\d+);/g, function(match, dec) {
         return String.fromCharCode(dec);
-    });
+    })
+        .replace(/&quot;/g, '')
+        .replace(/&eacute;/g, 'é')
+        .replace(/&amp;/g, '"')
+        .replace(/&ldquo;/g, '"')
+        .replace(/&rdquo;/g, '"')
+        .replace(/&shy;/g, '-');
 }
 
 export const uriToYouTube = function (string){
